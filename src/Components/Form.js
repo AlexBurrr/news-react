34e424ccb5063f9c4c4caa509dfcbb287241e8bb
Alex Berumen <alexandroberumen@gmail.com>
2021-04-07 18:27:00 -0700

    project is finished, but pages continue to have issues with loading the image

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react'
 import SignUpLogIn from '../Pages/SignUpLogIn';
 import FormSuccess from './FormSucess'
+import styled from 'styled-components'
+
+const FormContainer = styled.div`
+position: absolute;
+left: 50%;
+top: 50%;
+transform: translate(-50%, -50%);
+text-align: left;
+border-radius: .5rem;
+height: 40rem;
+width: 30rem;
+box-shadow: 4px 7px 15px 5px rgba(0,0,0,0.15);
+
+
+
+`
+
 
 const Form = () => {
 
@@ -11,9 +28,9 @@ const Form = () => {
     }
 
     return (
-        <div>
+        <FormContainer>
             {!isSubmitted ? <SignUpLogIn submitForm={submitForm} /> : <FormSuccess />}
-        </div>
+        </FormContainer>
     )
 }
 
--- src/Pages/SignUpLogIn.js
+++ src/Pages/SignUpLogIn.js
@@ -5,20 +5,56 @@ import validate from '../Components/validateInfo'
 
 
 const FormContent = styled.div`
+position: relative;
+text-align: left;
 width: 100%;
-text-align: center;
+
+height: 100%;
+
+
+
 `
 const Form = styled.form`
 p{
     color: red;
 }
 `
-const FormTitle = styled.h1``
-const FormInputs = styled.div``
-const FormLabel = styled.label``
-const FormInput = styled.input``
-const FormButton = styled.button``
-const Already = styled.span``
+const FormTitle = styled.h1`
+text-align:center;
+position: relative; 
+top: 1rem;
+padding-bottom: 2rem;
+font-family: 'Lato';
+
+`
+const FormInputs = styled.div`
+position: relative;
+left: 10%;
+
+
+`
+const FormLabel = styled.label`
+position: relative;
+left: 10%;`
+const FormInput = styled.input`
+position: relative;
+margin-bottom: 2rem;
+width: 80%;
+height: 3rem;
+`
+const FormButton = styled.button`
+position: relative;
+left: 10%;
+width: 80%;
+height: 3rem;
+`
+const Already = styled.span`
+position: absolute;
+left: 70%;
+top: 95%;
+transform: translate(-50%, -50%);
+width:100%;
+`
 
 
 
@@ -34,26 +70,24 @@ const SignUpLogIn = ({ submitForm }) => {
     return (
         <FormContent>
             <Form onSubmit={handleSubmit}>
-                <FormTitle>get started today</FormTitle>
+                <FormTitle>Get started today</FormTitle>
+                <FormLabel htmlFor='username'>Username: </FormLabel>
                 <FormInputs>
-                    <FormLabel htmlFor='username'>Username </FormLabel>
                     <FormInput id='username' type='text' name='username' placeholder='enter username' value={values.username} onChange={handleChange} />
                 </FormInputs>
                 {errors.username && <p>{errors.username}</p>}
+                <FormLabel htmlFor='email'>Email: </FormLabel>
                 <FormInputs>
-                    <FormLabel htmlFor='email'>email </FormLabel>
                     <FormInput id='email' type='email' name='email' placeholder='enter email' value={values.email} onChange={handleChange} />
                 </FormInputs>
                 {errors.email && <p>{errors.email}</p>}
-
+                <FormLabel htmlFor='password'>Password: </FormLabel>
                 <FormInputs>
-                    <FormLabel htmlFor='password'>password </FormLabel>
                     <FormInput id='password' type='password' name='password' placeholder='enter password' value={values.password} onChange={handleChange} />
                 </FormInputs>
                 {errors.password && <p>{errors.password}</p>}
-
+                <FormLabel htmlFor='confirm password'>Confirm password: </FormLabel>
                 <FormInputs>
-                    <FormLabel htmlFor='confirm password'>confirm password </FormLabel>
                     <FormInput id='password2' type='password' name='password2' placeholder='enter confirm password' value={values.password2} onChange={handleChange} />
                 </FormInputs>
                 {errors.password2 && <p>{errors.password2}</p>}
